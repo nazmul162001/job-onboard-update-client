77fc840ff29ea53bd5d8eba9050d47d26adaa75a
nazmul162001 <nazmulprime162001@gmail.com>
2022-09-18 06:58:39 +0600

    added card payment error handling

--- src/pages/PaymentSystem/Payment/CheckoutForm.jsx
+++ src/pages/PaymentSystem/Payment/CheckoutForm.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
 
 const CheckoutForm = () => {
   const stripe = useStripe();
   const elements = useElements();
+  const [cardError, setCardError] = useState("");
   const handleSubmit = async (event) => {
     event.preventDefault();
     if (!stripe || !elements) {
@@ -14,30 +15,54 @@ const CheckoutForm = () => {
     if (card === null) {
       return;
     }
+    // err handling for payment
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
+      card,
+    });
+    // if (error) {
+    //   setCardError(error.message);
+    // }
+    // else{
+    //   setCardError("");
+    // }
+    setCardError(error?.message || "");
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <CardElement
-        options={{
-          style: {
-            base: {
-              fontSize: "16px",
-              color: "#424770",
-              "::placeholder": {
-                color: "#aab7c4",
+    <>
+      <form onSubmit={handleSubmit}>
+        <CardElement
+          options={{
+            style: {
+              base: {
+                fontSize: "16px",
+                color: "#424770",
+                "::placeholder": {
+                  color: "#aab7c4",
+                },
+              },
+              invalid: {
+                color: "#9e2146",
               },
             },
-            invalid: {
-              color: "#9e2146",
-            },
-          },
-        }}
-      />
-      <button className="btn btn-primary w-full my-3" type="submit" disabled={!stripe}>
-        Make a Payment <span><i class="ri-bank-card-fill text-lg font-medium ml-2"></i></span>
-      </button>
-    </form>
+          }}
+        />
+        <button
+          className="btn btn-primary w-full my-3"
+          type="submit"
+          disabled={!stripe}
+        >
+          Make a Payment{" "}
+          <span>
+            <i class="ri-bank-card-fill text-lg font-medium ml-2"></i>
+          </span>
+        </button>
+      </form>
+      {
+        cardError && <p className="text-red-500 text-sm text-center">{cardError}</p>
+      }
+    </>
   );
 };
 
