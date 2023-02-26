09b88c7281506c2c1854ab3905e67173e5581f2d
nazmulhassan-tmpl <nazmul.h@tunicalabsmedia.com>
2023-02-26 20:20:53 +0600

    update api

--- src/config.js
+++ src/config.js
@@ -1,2 +1,3 @@
 // export const BASE_API = "http://localhost:5000";
-export const BASE_API = "https://job-on-board-server.vercel.app";
+// export const BASE_API = "https://job-on-board-server.vercel.app";
+export const BASE_API = "https://job-onboard-server-production.up.railway.app";
