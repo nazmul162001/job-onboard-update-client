2f50fb2f5cae93ef3495fdbf498da4cad5b5d341
Nazmul Hasan <nazmulprime162001@gmail.com>
2023-07-22 01:10:22 +0600

    update live link

--- src/config.js
+++ src/config.js
@@ -4,8 +4,8 @@
 
 
 // =============== nazmul api =================
-// export const BASE_API = "https://job-on-board-server-webdevnazmulh-gmailcom.vercel.app";
-export const BASE_API = 'http://localhost:5000'
+export const BASE_API = "https://job-on-board-server-webdevnazmulh-gmailcom.vercel.app";
+// export const BASE_API = 'http://localhost:5000'
 
 
 // ================ New api =================
